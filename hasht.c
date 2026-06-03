c7a78e1231a8b19fb987e91897b1f2013a3a6284
Ege Eskisar <egeeskisar@gmail.com>
2026-06-04 00:05:40 +0300

    better hashing

--- hasht.c
+++ hasht.c
@@ -1,5 +1,4 @@
 #include "hasht.h"
-#include <ctype.h>
 #define uint unsigned int
 #define min(a, b) ((a) < (b) ? (a) : (b))
 static const ushort lookup = (1 << 12) - 1;
@@ -9,10 +8,10 @@ static inline int mlen(char * __restrict p) {
     return p - s - 1;
 }
 ushort hash16(char * __restrict p) {
-    ushort k = (ushort)0;
+    int k = 0;
     char until = min(mlen(p), 16);
     for (char i = 0; i < until; i++) {
-        k += tolower(p[i]) - 'a';
+        k += p[i] - 'a';
     }
     //DIST = 2^12
     return (((uint)k * until) ^ -1) & lookup;
